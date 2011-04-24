ead7e83df80a51e967d0268dcf019a53e1565b90
Miron Brezuleanu <mbrezu@gmail.com>
2011-04-24 21:15:55 +0300

    First experiments with text.

--- scenic.lisp
+++ scenic.lisp
@@ -14,6 +14,17 @@
                            'sdl-cffi::Sdl-Surface
                            'sdl-cffi::pitch))
 
+(defun font-extents ()
+  (let ((extents (cl-cairo2:get-font-extents)))
+    `((ascent . ,(cl-cairo2:font-ascent extents))
+      (descent . ,(cl-cairo2:font-descent extents)))))
+
+(defun text-extents (text)
+  (mapcar #'(lambda (name dim)
+              (cons name dim))
+          (multiple-value-list (cl-cairo2:text-extents text))
+          '(x_bearing y_bearing width height x_advance y_advance)))
+
 (defun cairo-test ()
   (let* ((sdl-surf (sdl:create-surface 800 600))
          (surf (cl-cairo2:create-image-surface-for-data (pixels-of sdl-surf)
@@ -30,6 +41,23 @@
     (cl-cairo2:set-source-rgb 1 0.3 0.3)
     (cl-cairo2:set-line-width 1)
     (cl-cairo2:stroke)
+    
+    (cl-cairo2:set-font-size 40)
+    (cl-cairo2:set-source-rgb 0 0 0)
+    (cl-cairo2:select-font-face "Helvetica" :normal :normal)
+    (cl-cairo2:set-line-width 1)
+    (cl-cairo2:move-to 10 100)
+    
+    (let ((text "Ana are mere proaspete."))
+      (cl-cairo2:show-text text)
+      (multiple-value-bind
+            (x_bearing y_bearing width height x_advance y_advance) (cl-cairo2:text-extents text)
+        (cl-cairo2:rectangle 10.5  (+ 100.5 y_bearing) width height)
+        (cl-cairo2:stroke))
+      (format t "~a~%" (text-extents text))
+      (format t "~a~%" (font-extents)))
+    
+    
     ;; (cl-cairo2:rectangle 0.5 0.5 200.5 100.5)
     ;; (cl-cairo2:set-source-rgb 0.3 0.7 0.7)
     ;; (cl-cairo2:fill-preserve)
